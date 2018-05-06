0db672436bfc4764c67fcffea572769c01a71126
mgorav <mgorav@gmail.com>
2018-05-06 10:12:04 +0200

    rest support framework

--- jpa-eclipselink-spring-boot-autoconfigure/src/main/java/com/gm/shared/jpa/eclipselink/rest/project/marshaller/ObjectMarshaller.java
+++ jpa-eclipselink-spring-boot-autoconfigure/src/main/java/com/gm/shared/jpa/eclipselink/rest/project/marshaller/ObjectMarshaller.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Marshaller {
+public class ObjectMarshaller {
 
     @Autowired
     private ProjectService projectService;
